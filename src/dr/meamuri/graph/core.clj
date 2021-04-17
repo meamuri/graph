e8033188eaaeb4323c29de12b83b74cce9a28c2d
Roman Dronov <meammf@yandex.ru>
2021-04-17 19:57:53 +0300

    Remove rewritten code

--- src/dr/meamuri/graph/core.clj
+++ src/dr/meamuri/graph/core.clj
@@ -7,18 +7,8 @@
         :3 [:4]
         :4 []})
 
-(defn traverse-graph-dfs [g s]
-  (loop [vertices [] explored #{s} frontier [s]]
-    (if (empty? frontier)
-      vertices
-      (let [v (peek frontier)
-            neighbors (g v)]
-        (recur
-         (conj vertices v)
-         (into explored neighbors)
-         (into (pop frontier) (remove explored neighbors)))))))
-
-(defn seq-graph [d g s]
+(defn seq-graph
+  [d g s]
   ((fn rec-seq [explored frontier]
      (lazy-seq
       (if (empty? frontier)
@@ -33,6 +23,5 @@
 (def seq-graph-dfs (partial seq-graph []))
 (def seq-graph-bfs (partial seq-graph (clojure.lang.PersistentQueue/EMPTY)))
 
-(traverse-graph-dfs G :1) ; => [:1 :3 :4 :2]
 (seq-graph-dfs G :1) ; => (:1 :3 :4 :2)
 (seq-graph-bfs G :1) ; => (:1 :2 :3 :4)
