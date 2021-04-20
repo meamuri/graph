035b5359d744921ee9ca88a3c56f0412489f60d4
Roman Dronov <meammf@yandex.ru>
2021-04-20 16:46:50 +0300

    Add dev paths

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject dr.meamuri/graph "0.1.0"
   :description "Tiny graph operations lib"
-  :dependencies [[org.clojure/clojure "1.10.3"]])
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+
+  :profiles
+  {:dev
+   {:source-paths ["env/dev"]}})
