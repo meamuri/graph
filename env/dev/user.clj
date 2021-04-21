54cd58b8e81af7d685c34a3c3f2426aa1694dee6
Roman Dronov <meammf@yandex.ru>
2021-04-21 22:31:04 +0300

    Add traversing style for graph operations (#5)
    * Add traversing style for graph operations
    
    * Generalize reduce traversing
    
    * Fix args order

--- env/dev/user.clj
+++ env/dev/user.clj
@@ -1,25 +1,16 @@
 (ns user
   (:require [dr.meamuri.graph.path :refer [shortest-path]]
             [dr.meamuri.graph.random :refer [make-graph]]
-            [dr.meamuri.graph.ops :refer [eccentricity diameter radius]]))
+            [dr.meamuri.graph.ops :refer [eccentricity diameter radius]]
+            [dr.meamuri.graph.core :refer [seq-graph-dfs seq-graph-bfs]]))
 
-;;
-;;
-;;       \
-;;      / \
-;;     /   \
-;;    /     \
-;;   |       \
-;;   | [5]   |
-;;    /   \  |
-;;   |     [4]
-;;   |    /
-;;   | [3]
-;;    /
-;; [1]
-;;    \
-;;     [2]
-;;
+;; v means vertex, w means weight
+(def G*
+  {:1 [{:v :2 :w 2} {:v :3 :w 3}]
+   :2 [{:v :4 :w 4}]
+   :3 [{:v :4 :w 1}]
+   :4 []})
+
 (def G
   {:1 [{:v :2 :w 2} {:v :3 :w 7} {:v :5 :w 3} {:v :4 :w 7}]
    :2 []
@@ -28,22 +19,23 @@
    :5 [{:v :4 :w 2}]})
 
 (comment
+  (seq-graph-dfs G* :1)
+  (seq-graph-bfs G* :1)
+
   ((fn []
      (-> G
          (shortest-path :1 :4))))
   ((fn []
-     (-> (make-graph 5 7)
-         (shortest-path :1 :4))))
+     (let [g (make-graph 5 7)
+           p (shortest-path g :1 :4)]
+       (println (str "Graph is " g))
+       (println (str "Shortest path between :1 and :4 is " p)))))
 
-  (#(-> (make-graph 3 4)
-        (eccentricity :1)))
-
-  (#(let [g (make-graph 5 12)
-          d (diameter g)]
-      (println (str "Graph is " g))
-      (println (str "Diameter for graph is " d))))
-
-  (#(let [g (make-graph 3 4)
+  (#(let [g (make-graph 4 7)
+          e (eccentricity g :1)
+          d (diameter g)
           r (radius g)]
       (println (str "Graph is " g))
+      (println (str "Eccentricity for graph is " e))
+      (println (str "Diameter for graph is " d))
       (println (str "Radius for graph is " r)))))
