4dd167648a24f91f76ed28d60cc1da5f2fa3808c
Roman Dronov <meammf@yandex.ru>
2021-04-21 13:53:46 +0300

    Basic layout for graph operations module

--- env/dev/user.clj
+++ env/dev/user.clj
@@ -2,13 +2,6 @@
   (:require [dr.meamuri.graph.path :refer [shortest-path]]
             [dr.meamuri.graph.random :refer [make-graph]]))
 
-(def Graph {:1 [{:v :2, :w 1} {:v :5, :w 102} {:v :4, :w 12}]
-            :2 [{:v :3, :w 100} {:v :6, :w 50}]
-            :3 [{:v :4, :w 1}]
-            :4 []
-            :5 [{:v :4, :w 5}]
-            :6 [{:v :3, :w 1}]})
-
 ;;
 ;;
 ;;       \
@@ -36,11 +29,7 @@
 (comment
   ((fn []
      (-> G
-         (shortest-path :1 :4)
-         str
-         println)))
+         (shortest-path :1 :4))))
   ((fn []
      (-> (make-graph 5 7)
-         (shortest-path :1 :6)
-         str
-         println))))
+         (shortest-path :1 :4)))))
---
+++ src/dr/meamuri/graph/ops.clj
@@ -0,0 +1,18 @@
+(ns dr.meamuri.graph.ops
+  "A suite of functions to calculate distance properties for the graph"
+  {:author "Roman Dronov"})
+
+(defn eccentricity
+  "The eccentricity of a vertex v is defined as the greatest distance between v and any other vertex."
+  [graph source]
+  0)
+
+(defn radius
+  "The radius of a graph is the minimum eccentricity of any vertex in a graph."
+  [graph]
+  0)
+
+(defn diameter
+  "The diameter of a graph is the maximum eccentricity of any vertex in a graph."
+  [graph]
+  0)
